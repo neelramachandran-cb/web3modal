b3e24a51359f82959fe2153d580116d6fff5acae
enesozturk <enesozturk.d@gmail.com>
2023-11-22 20:59:59 +0300

    chore: fixes

--- packages/scaffold/src/views/w3m-account-view/index.ts
+++ packages/scaffold/src/views/w3m-account-view/index.ts
@@ -232,7 +232,7 @@ export class W3mAccountView extends LitElement {
               assets: ['USDC']
             }
           ],
-          partnerUserId: ''
+          partnerUserId: address
         },
         experienceLoggedIn: 'popup',
         experienceLoggedOut: 'popup',
